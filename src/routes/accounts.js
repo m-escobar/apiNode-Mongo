46b6c9e104ff32fb644e22c890c8e9b1c3f45be2
Marcelo Escobar <escobar@br.systems>
2020-07-01 17:01:08 -0300

    added agency balance

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -39,6 +39,29 @@ router.get('/balance', async (req, res) => {
   };
 });
 
+router.get('/agencybalance', async (req, res) => {
+  try {
+    let request = req.body;
+    const agency = parseInt(request.agency, 10);
+    let result = null;
+
+    const account_info = await accountsModel.aggregate(
+      [{'$match': { 'agency': agency }}, {'$group': { '_id': null, 'balance': {'$avg': '$balance'}}}]
+      );
+      
+    if(account_info[0] !== undefined) {
+      result = `Balance: ${account_info[0]['balance']}`;
+    } else {
+      result = `Check agency number`;
+    }
+
+    res.send(`${result}`);
+  } catch (err) {
+    res.status(400).send({ error: err.message});
+    console.log(`GET /account/agencybalance - ${err.message}`);
+  };
+});
+
 router.post('/transfer', async (req, res) => {
   try {
     let request = req.body;
@@ -84,7 +107,7 @@ router.post('/transfer', async (req, res) => {
     res.send(`${result}`);
   } catch (err) {
     res.status(400).send({ error: err.message});
-    console.log(`GET /account/balance - ${err.message}`);
+    console.log(`GET /account/transfer - ${err.message}`);
   };
 });
 
@@ -141,7 +164,7 @@ router.put('/withdraw', async (req, res) => {
     res.send(`${result}`);
     } catch (err) {
       res.status(400).send({ error: err.message});
-      console.log(`PUT /account - ${err.message}`);
+      console.log(`PUT /account/withdraw - ${err.message}`);
     };
 });
 
@@ -168,61 +191,8 @@ router.delete('/', async (req, res) => {
     res.send(`${result}`);
   } catch (err) {
     res.status(400).send({ error: err.message});
-    console.log(`GET /account - ${err.message}`);
+    console.log(`DELETE /account - ${err.message}`);
   };
 });
-
-// router.post('/', async (req, res) => {
-//   let account = req.body;
-//   try {
-//     let data = await fs.readFile(global.fileName, 'utf-8');
-//     let json = JSON.parse(data);
-
-//     account = { id: json.nextId, ...account};
-//     json.nextId++;
-//     json.accounts.push(account);
-
-//     await fs.writeFile(global.fileName, JSON.stringify(json));
-    
-//     res.send({"new_account": account});
-//   } catch (err) {
-//       res.status(400).send({ error: err.message });
-//       console.log(`POST /account - ${err.message}`);
-//   }
-// });
-
-// router.get('/:id', async (req, res) => {
-//   try {
-//     let data = await fs.readFile(global.fileName, 'utf-8');
-//     let json = JSON.parse(data);
-//     const account = json.accounts.find(account => account.id === parseInt(req.params.id, 10));
-
-//     if (account) {
-//       res.send(account);
-//     } else {
-//       res.status(400).send({error: 'Account not found'});
-//       console.log('GET /account - Account not found');
-//     }
-//   } catch {
-//     res.status(400).send({ error: err.message});
-//     console.log(`GET /account/:id - ${err.message}`);
-//   };
-// });
-
-// router.delete('/:id', async (req, res) => {
-//   try {
-//     let data = await fs.readFile(global.fileName, 'utf-8');
-//     let json = JSON.parse(data);
-//     let accounts = json.accounts.filter(account => account.id !== parseInt(req.params.id, 10));
-//     json.accounts = accounts;
-
-//     await fs.writeFile(global.fileName, JSON.stringify(json));
-//     res.send('account deleted');
-//   } catch {
-//       res.status(400).send({ error: err.message});
-//       console.log(`DELETE /account/:id - ${err.message}`);
-//     };
-// });
-
 
 module.exports = router;
